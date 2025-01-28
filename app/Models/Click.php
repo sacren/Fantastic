450175ecddfd9bc63ff20e1c144318b2ecc48bcf
Jean Sacren <sakiwit@gmail.com>
2025-01-28 04:20:13 -0700

    Update the migration of Click model
    Add the list of columns to clicks table and they are mass assignable.

--- app/Models/Click.php
+++ app/Models/Click.php
@@ -9,4 +9,15 @@ class Click extends Model
 {
     /** @use HasFactory<\Database\Factories\ClickFactory> */
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'listing_id',
+        'user_agent',
+        'ip_address',
+    ];
 }
--- database/migrations/2025_01_28_035217_create_clicks_table.php
+++ database/migrations/2025_01_28_035217_create_clicks_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Listing;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,11 @@ return new class extends Migration
     {
         Schema::create('clicks', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(model: Listing::class)
+                  ->constrained()
+                  ->cascadeOnDelete();
+            $table->text(column: 'user_agent')->nullable();
+            $table->string(column: 'ip_address')->nullable();
             $table->timestamps();
         });
     }
