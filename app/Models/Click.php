ccc105bb254e66f6fc8c6e48ade2a576fe212c51
Jean Sacren <sakiwit@gmail.com>
2025-01-29 00:14:30 -0700

    Add Eloquent relationships between Listing and Click models
    In the Click model, define a belongsTo relationship with the Listing
    model.
    
    In the Listing model, define a hasMany relationship with the Click
    model.

--- app/Models/Click.php
+++ app/Models/Click.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Click extends Model
 {
@@ -20,4 +21,21 @@ class Click extends Model
         'user_agent',
         'ip_address',
     ];
+
+    /**
+     * Get the listing that owns the click.
+     *
+     * This method defines a belongsTo relationship between the Click model and
+     * the Listing model.
+     *
+     * It allows to retrieve the associated listing for a given click.  We can
+     * use this relationship to access properties and methods of the related
+     * listing, such as `$click->listing->title`.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function listing(): BelongsTo
+    {
+        return $this->belongsTo(Listing::class);
+    }
 }
--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Listing extends Model
 {
@@ -26,6 +27,28 @@ class Listing extends Model
         'content',
         'apply_link',
     ];
+
+    /**
+     * Get the clicks associated with the listing.
+     *
+     * This method defines a one-to-many relationship between the Listing model
+     * and the Click model.
+     *
+     * It allows to retrieve all the clicks that have been made on a particular
+     * listing.
+     *
+     * Each listing can have multiple clicks, and this method provides an easy
+     * way to access those clicks.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     * Returns an instance of the HasMany relationship, which can be used to
+     * query the associated clicks.  We can chain additional query methods such
+     * as where(), orderBy(), etc., to further filter or sort the clicks.
+     */
+    public function clicks(): HasMany
+    {
+        return $this->hasMany(Listing::class);
+    }
 
     /**
      * Get the user that owns the listing.
