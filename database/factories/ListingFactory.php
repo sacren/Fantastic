5d6ac9c77eb01f8c3d9073068915f5997d688c47
Jean Sacren <sakiwit@gmail.com>
2025-01-30 02:31:15 -0700

    Build the factory for Listing model
    Update ListingFactory factory with the required columns in alignment
    with Listing model migration.

--- database/factories/ListingFactory.php
+++ database/factories/ListingFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Listing>
@@ -16,8 +18,25 @@ class ListingFactory extends Factory
      */
     public function definition(): array
     {
+        $title = fake()->jobTitle();
+        $paragraph = fake()->paragraphs(3);
+        $content = '';
+
+        foreach ($paragraph as $line) {
+            $content .= '<p class="mt-4">' . $line . '</p>';
+        }
+
         return [
-            //
+            'user_id' => User::factory(),
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'company' => fake()->company(),
+            'location' => fake()->city(),
+            'logo' => basename(fake()->image(storage_path('app/public'))),
+            'is_highlighted' => fake()->boolean(70),
+            'is_active' => true,
+            'content' => $content,
+            'apply_link' => fake()->url(),
         ];
     }
 }
