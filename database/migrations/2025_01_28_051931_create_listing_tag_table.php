616dae1b5b43c905dfcd5df84964003d67d03f2c
Jean Sacren <sakiwit@gmail.com>
2025-01-29 17:17:44 -0700

    Update the migration for the pivot table
    Build the pivot table for Listing and Tag models.
    
    Use the constrained() method to create a foreign key constraint that
    references the id column of the listings and tags tables.
    
    We have a listings table and a tags table, and we have a many-to-many
    relationship between them using a pivot table.  Use cascadeOnDelete()
    method to delete a listing.  If the listing is deleted, we want to
    automatically delete all the related records in the pivot table that
    reference that listing.

--- database/migrations/2025_01_28_051931_create_listing_tag_table.php
+++ database/migrations/2025_01_28_051931_create_listing_tag_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Listing;
+use App\Models\Tag;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,7 +14,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('listing_tag', function (Blueprint $table) {
-            $table->id();
+            $table->foreignIdFor(Listing::class)
+                  ->constrained()
+                  ->cascadeOnDelete();
+            $table->foreignIdFor(Tag::class)
+                  ->constrained()
+                  ->cascadeOnDelete();
             $table->timestamps();
         });
     }
