ce23717dd020b64ad66abbd3eb971c1a0701ff60
Jean Sacren <sakiwit@gmail.com>
2025-02-09 04:55:02 -0700

    Build the landing page for Listing model

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -12,5 +12,37 @@
                 @endforeach
             </div>
         </div>
+
+        <div class="mb-12">
+            <h2 class="text-2xl font-medium text-gray-900 title-font px-4">
+                All Listings ({{ $listings->count() }})
+            </h2>
+        </div>
+        <div class="my-6">
+            @foreach ($listings as $listing)
+                <a href="#"
+                    class="py-6 px-4 flex flex-wrap md:flex-nowrap border-b border-gray-100 {{ $listing->is_highlighted ? 'bg-yellow-100 hover:bg-yellow-200' : 'bg-white hover:bg-gray-100' }}">
+                    <div class="md:w-16 md:mb-0 mb-6 mr-4 flex-shrink-0 flex flex-col">
+                        <img src="/storage/{{ $listing->logo }}" alt="{{ $listing->company }} logo"
+                            class="w-16 h-16 rounded-full object-cover">
+                    </div>
+                    <div class="md:w-1/2 mr-8 flex flex-col items-start justify-center">
+                        <h2 class="text-xl font-bold text-gray-900 title-font mb-1">{{ $listing->title }}</h2>
+                        <p class="leading-relaxed">
+                            {{ $listing->company }} &mdash;
+                            <span class="text-gray-600">{{ $listing->location }}</span>
+                        </p>
+                    </div>
+                    <div class="md:flex-grow mr-8 flex items-center justify-start">
+                        @foreach ($listing->tags as $tag)
+                            <span
+                                class="inline-block ml-2 tracking-wide text-xs font-medium title-font px-1.5 py-0.5 border border-indigo-500">
+                                {{ $tag->name }}
+                            </span>
+                        @endforeach
+                    </div>
+                </a>
+            @endforeach
+        </div>
     </section>
 </x-app-layout>
