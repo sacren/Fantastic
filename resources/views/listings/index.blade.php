a119c393b39bc80f57ce934c8dbff4ffdd8685be
Jean Sacren <sakiwit@gmail.com>
2025-02-08 22:24:34 -0700

    Add named tags
    Build tags collection and pass it to the view.
    
    Build the list of tag names and put it below the hero.

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -1,3 +1,16 @@
 <x-app-layout>
     <x-hero></x-hero>
+
+    <section class="container px-5 py-12 mx-auto">
+        <div class="mb-12">
+            <div class="flex justify-center">
+                @foreach ($tags as $tag)
+                    <a href="{{ route('listings.index', ['tag' => $tag->slug]) }}"
+                        class="inline-block ml-2 tracking-wide text-xs font-medium title-font px-1.5 py-0.5 border border-indigo-500 uppercase">
+                        {{ $tag->name }}
+                    </a>
+                @endforeach
+            </div>
+        </div>
+    </section>
 </x-app-layout>
