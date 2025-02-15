247f6220756b5671aebb12c6795f772c964a6e92
Jean Sacren <sakiwit@gmail.com>
2025-02-15 16:10:18 -0700

    Remove ineffective Tailwind utilities
    Clean up those Tailwind utilities that are in use but ineffective, yet
    the hero section retains the same look.

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -7,10 +7,10 @@
                 <div class="relative mr-4 lg:w-full xl:w-1/2 w-2/4">
                     <input type="text" id="s" name="s" value="{{ request()->get('s') }}"
                         placeholder="Search for jobs..."
-                        class="w-full bg-gray-100 bg-opacity-50 rounded-lg border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 focus:bg-white transition duration-200 ease-in-out text-base outline-none text-gray-700 px-4 py-2 shadow-sm hover:shadow-md">
+                        class="w-full bg-gray-100 rounded-lg border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 focus:bg-white transition duration-200 ease-in-out text-base text-gray-700 px-4 py-2 shadow-sm hover:shadow-md">
                 </div>
                 <button type="submit"
-                    class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded-lg text-lg transition duration-200 ease-in-out transform hover:scale-105 shadow-md hover:shadow-lg">
+                    class="text-white bg-indigo-500 border-0 py-2 px-6 hover:bg-indigo-600 rounded-lg text-lg transition duration-200 ease-in-out transform hover:scale-105 shadow-md hover:shadow-lg">
                     Search
                 </button>
             </form>
