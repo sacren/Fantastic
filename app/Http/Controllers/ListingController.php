761d1b136f85808a93dd2dde36f415cf3ceb90d5
Jean Sacren <sakiwit@gmail.com>
2025-02-23 01:05:47 -0700

    Add create view of Listing model
    Build the create view scaffolding using make:view.
    
    $ sail artisan make:view -- listings.create
    
       INFO  View [resources/views/listings/create.blade.php] created successfully.
    
    Build the view by applying x-app-layout.  Use flex-grow to ensure the
    section expands to fill the remaining space between the header and
    footer.
    
    Build the create action by the use of view helper.

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -46,7 +46,7 @@ class ListingController extends Controller
      */
     public function create()
     {
-        //
+        return view('listings.create');
     }
 
     /**
---
+++ resources/views/listings/create.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <section class="text-gray-600 body-font overflow-hidden flex-grow">
+        <div class="w-full md:w-1/2 py-24 mx-auto">
+            <div class="mb-4">
+                <h1 class="text-2xl font-medium text-gray-900 title-font">
+                    Create a new listing for only $99.99!
+                </h1>
+            </div>
+        </div>
+    </section>
+</x-app-layout>
