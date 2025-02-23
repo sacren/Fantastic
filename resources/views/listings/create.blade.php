294028c05f24a841baa03f97597888380cf18709
Jean Sacren <sakiwit@gmail.com>
2025-02-23 16:57:31 -0700

    Add the display of errors
    In the process of adding a new job listing, if errors occur, we will
    display the errors to alert the user.

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -6,6 +6,22 @@
                     Create a new listing for only $99.99!
                 </h1>
             </div>
+            @if ($errors->any())
+                <div class="mb-4 p-4 bg-red-50 border-l-4 border-red-500">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li class="text-red-500 flex items-center">
+                                <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd"
+                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zM9 9a1 1 0 012 0v4a1 1 0 01-2 0V9zm1-5a1 1 0 00-1 1v1a1 1 0 002 0V5a1 1 0 00-1-1z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
+                                {{ $error }}
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
         </div>
     </section>
 </x-app-layout>
