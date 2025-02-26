269ac56858fb5ed3861d85d6b8ba1d0c6ddf0afd
Jean Sacren <sakiwit@gmail.com>
2025-02-26 15:51:15 -0700

    Add the rest of the fields of create view
    Build the rest of the fields and complete the layout of the create view.
    
    Spruce up the entire create view.

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
     <section class="text-gray-600 body-font overflow-hidden flex-grow">
         <div class="w-full md:w-1/2 py-24 mx-auto">
-            <div class="mb-4">
+            <div class="mb-8">
                 <h1 class="text-2xl font-medium text-gray-900 title-font">
                     Create a new listing for only $99.99!
                 </h1>
             </div>
             @if ($errors->any())
-                <div class="mb-4 p-4 bg-red-50 border-l-4 border-red-500">
+                <div class="mb-6 p-4 bg-red-50 border-l-4 border-red-500 transition-all duration-300 ease-in-out">
                     <ul>
                         @foreach ($errors->all() as $error)
                             <li class="text-red-500 flex items-center">
@@ -23,28 +23,119 @@
                 </div>
             @endif
             <form action="{{ route('listings.store') }}" method="post" id="payment-form" enctype="multipart/form-data"
-                class="bg-gray-100 p-4">
+                class="bg-gray-100 p-6 rounded-lg">
                 @guest
-                    <div class="flex flex-wrap mb-4">
-                        <div class="w-full md:w-1/2 px-2 mb-4">
+                    <div class="flex flex-wrap mb-6">
+                        <!-- Email Field -->
+                        <div class="w-full md:w-1/2 px-3 mb-6">
                             <x-input-label for="email" value="{{ __('Email Address') }}"></x-input-label>
-                            <input class="block mt-1 w-full" id="email" type="text" name="email"
-                                :value="old('email')" required autofocus autocomplete="username">
+                            <input
+                                class="block mt-1 w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                id="email" type="text" name="email" :value="old('email')" required autofocus
+                                autocomplete="username">
                         </div>
-                        <div class="w-full md:w-1/2 px-2 mb-4">
+
+                        <!-- Full Name Field -->
+                        <div class="w-full md:w-1/2 px-3 mb-6">
                             <x-input-label for="name" value="{{ __('Full Name') }}"></x-input-label>
-                            <input class="block mt-1 w-full" id="name" type="text" name="name"
-                                :value="old('name')" required autofocus autocomplete="username">
+                            <input
+                                class="block mt-1 w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                id="name" type="text" name="name" :value="old('name')" required
+                                autocomplete="username">
                         </div>
-                        <div class="w-full md:w-1/2 px-2 mb-4">
+
+                        <!-- Password Field -->
+                        <div class="w-full md:w-1/2 px-3 mb-6">
                             <x-input-label for="password" value="{{ __('Password') }}"></x-input-label>
-                            <input class="block mt-1 w-full" id="password" type="password" name="password" required
+                            <input
+                                class="block mt-1 w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                id="password" type="password" name="password" required autocomplete="new-password">
+                        </div>
+
+                        <!-- Confirm Password Field -->
+                        <div class="w-full md:w-1/2 px-3 mb-6">
+                            <x-input-label for="password_confirmation" value="{{ __('Confirm Password') }}"></x-input-label>
+                            <input
+                                class="block mt-1 w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                id="password_confirmation" type="password" name="password_confirmation" required
                                 autocomplete="new-password">
                         </div>
-                        <div class="w-full md:w-1/2 px-2 mb-4">
-                            <x-input-label for="password_confirmation" value="{{ __('Confirm Password') }}"></x-input-label>
-                            <input class="block mt-1 w-full" id="password_confirmation" type="password"
-                                name="password_confirmation" required autocomplete="new-password">
+
+                        <!-- Job Title Field -->
+                        <div class="w-full px-3 mb-6">
+                            <x-input-label for="job_title" value="{{ __('Job Title') }}"></x-input-label>
+                            <input
+                                class="block mt-1 w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                id="job_title" type="text" name="job_title" required>
+                        </div>
+
+                        <!-- Company Name Field -->
+                        <div class="w-full px-3 mb-6">
+                            <x-input-label for="company_name" value="{{ __('Company Name') }}"></x-input-label>
+                            <input
+                                class="block mt-1 w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                id="company_name" type="text" name="company_name" required>
+                        </div>
+
+                        <!-- Company Logo Field -->
+                        <div class="px-3 mb-6">
+                            <x-input-label for="logo" value="{{ __('Company Logo') }}"></x-input-label>
+                            <input
+                                class="block mt-1 w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                type="file" id="logo" name="logo" required>
+                        </div>
+
+                        <!-- Location Field -->
+                        <div class="w-full px-3 mb-6">
+                            <x-input-label for="location" value="{{ __('Location (e.g. San Francisco)') }}"></x-input-label>
+                            <input
+                                class="block mt-1 w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                type="text" id="location" name="location" required>
+                        </div>
+
+                        <!-- Apply Link Field -->
+                        <div class="w-full px-3 mb-6">
+                            <x-input-label for="apply_link" value="{{ __('Apply Link') }}"></x-input-label>
+                            <input
+                                class="block mt-1 w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                type="text" id="apply_link" name="apply_link" required>
+                        </div>
+
+                        <!-- Tags Field -->
+                        <div class="w-full px-3 mb-6">
+                            <x-input-label for="tags" value="{{ __('Tags') }}"></x-input-label>
+                            <input
+                                class="block mt-1 w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                type="text" id="tags" name="tags" required>
+                        </div>
+
+                        <!-- Listing Content Field -->
+                        <div class="w-full px-3 mb-6">
+                            <x-input-label for="content" value="{{ __('Listing Content') }}"></x-input-label>
+                            <textarea
+                                class="rounded-md shadow-sm border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 mt-1 w-full min-h-[150px]"
+                                id="content" name="content" required></textarea>
+                        </div>
+
+                        <!-- Highlight Checkbox -->
+                        <div class="w-full px-3 mb-6">
+                            <label for="is_highlighted" class="inline-flex items-center">
+                                <input id="is_highlighted" type="checkbox" name="is_highlighted" value="yes"
+                                    class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500 hover:border-indigo-500"
+                                    required>
+                                <span
+                                    class="ms-2 text-sm text-gray-600">{{ __('Highlight this post (extra $19.99)') }}</span>
+                            </label>
+                        </div>
+
+                        <!-- Submit Button -->
+                        <div class="w-full px-3 mb-6">
+                            @csrf
+                            <input id="payment-method" type="hidden" name="payment-method" value="">
+                            <button type="submit" id="submit-button"
+                                class="w-full px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-md text-white transition-all duration-300 ease-in-out">Pay
+                                +
+                                Continue</button>
                         </div>
                     </div>
                 @endguest
