3cc20d4e7e04aa9cdde7a64e4c8ac190dc490d5f
Jean Sacren <sakiwit@gmail.com>
2025-03-03 16:37:24 -0700

    Add authenticated user
    Add the authenticated user and let the user log in.

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class ListingController extends Controller
 {
@@ -74,6 +76,20 @@ class ListingController extends Controller
         }
 
         $request->validate($validateList);
+
+        $user = Auth::user();
+
+        if (!$user) {
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            $user->createAsStripeCustomer();
+
+            Auth::login($user);
+        }
 
         return redirect()->route('dashboard');
     }
